fbfca25ee50de18a9233015620fa7913a9e6e614
guotie <guotie.9@gmail.com>
2014-01-08 19:28:14 +0800

    add some tests.

--- gb2312.go
+++ gb2312.go
@@ -18,6 +18,42 @@ const (
 	UTF8_B3 = 0xe0
 )
 
+// param: input: input bytes array
+// return: output: output bytes array
+//         err: error if there are errors when convert
+//         ic: input has been converted
+//         oc: output has been converted
+func ConvertGB2312(input []byte) (output []byte, err error, ic int, oc int) {
+	ilen := len(input)
+	output = make([]byte, (ilen/2)*3+3)
+	olen := 0
+	for i := 0; i < ilen-1; {
+		if input[i] <= 0x7f {
+			output[olen] = input[i]
+			olen++
+			i++
+		} else {
+			gb := int(input[i])<<8 | int(input[i+1])
+			u8, ok := gb2312toutf8[gb]
+			if !ok {
+				err = fmt.Errorf("gb2312 has no character %x, at %d\n", gb, ilen)
+				ic = i
+				oc = olen
+				return
+			}
+			output[olen] = byte(u8 >> 16)
+			olen++
+			output[olen] = byte((u8 >> 8) & 0xff)
+			olen++
+			output[olen] = byte(u8 & 0xff)
+			olen++
+			i = i + 2
+		}
+	}
+	output = output[0:olen]
+	return output, nil, ilen, olen
+}
+
 func unicode2utf8(u int) int {
 	if u > 0x10000 {
 		panic(fmt.Sprintf("unicode for gb2312 is invalid: 0x%x\n", u))
@@ -41,17 +77,23 @@ func readcp936(fp string) {
 		fmt.Println(err.Error())
 		return
 	}
-	sf, err := os.Create("./data.tmp")
+	sf, err := os.Create("./gbk2utf8.tmp")
 	if err != nil {
 		panic(err.Error())
 	}
+	sf2, err2 := os.Create("./gbk2unicode.tmp")
+	if err2 != nil {
+		panic(err2.Error())
+	}
 
-	sf.WriteString("package gogb2312\n\nvar convertmap=map[int]int{")
+	sf.WriteString("package gogb2312\n\nvar gb2312toutf8=map[int]int{")
+	sf2.WriteString("package gogb2312\n\nvar gb2312tounicode=map[int]int{")
+
 	rd := bufio.NewReader(f)
 	i := 0
 	line, _, err := rd.ReadLine()
 	for ; err == nil; line, _, err = rd.ReadLine() {
-		gb, u8, e := parseline(line, i)
+		gb, unicode, u8, e := parseline(line, i)
 		if e != nil {
 			fmt.Print(e)
 			i++
@@ -62,6 +104,10 @@ func readcp936(fp string) {
 		if serr != nil {
 			fmt.Println("save code failed", serr)
 		}
+		serr = savecode(sf2, gb, unicode)
+		if serr != nil {
+			fmt.Println("save code failed", serr)
+		}
 	}
 	if err != nil && err != io.EOF {
 		fmt.Println(err)
@@ -69,6 +115,8 @@ func readcp936(fp string) {
 	fmt.Printf("Lines of %s: %d\n", fp, i)
 	sf.WriteString("}\n")
 	sf.Close()
+	sf2.WriteString("}\n")
+	sf2.Close()
 }
 
 func savecode(rd *os.File, gb, u8 int) error {
@@ -79,37 +127,37 @@ func savecode(rd *os.File, gb, u8 int) error {
 
 var re_space = regexp.MustCompile(`\s+`)
 
-func parseline(line []byte, i int) (int, int, error) {
+func parseline(line []byte, i int) (int, int, int, error) {
 	comma := bytes.Index(line, []byte{'#'})
 	if comma >= 0 {
 		line = line[0:comma]
 	}
 	if l := len(line); l < 4 {
-		return -1, -1, fmt.Errorf("line %d length invalid: %d %q\n", i, l, line)
+		return -1, -1, -1, fmt.Errorf("line %d length invalid: %d %q\n", i, l, line)
 	}
 	rl := bytes.TrimSpace(line)
 	rl = re_space.ReplaceAll(rl, []byte{' '})
 	chs := bytes.Split(rl, []byte{' '})
 	if len(chs) != 2 {
-		return -1, -1,
+		return -1, -1, -1,
 			fmt.Errorf("line %d has %d numbers: %s\n", i, len(chs), rl)
 	}
 	ret, err := strconv.ParseInt(string(bytes.ToLower(chs[0])), 0, 32)
 	if err != nil {
-		return -1, -1,
+		return -1, -1, -1,
 			fmt.Errorf("convert %q to int failed at line %d: %s\n", chs[0], i, err)
 	}
 	gb2312 := int(ret)
 	if gb2312 <= 0x7f {
-		return gb2312, gb2312, fmt.Errorf("No need convert for ascii 0x%x\n", gb2312)
+		return gb2312, gb2312, gb2312, fmt.Errorf("No need convert for ascii 0x%x\n", gb2312)
 	}
 	ret, err = strconv.ParseInt(string(bytes.ToLower(chs[1])), 0, 32)
 	if err != nil {
-		return -1, -1,
+		return -1, -1, -1,
 			fmt.Errorf("convert %q to int failed at line %d: %s\n", chs[1], i, err)
 	}
 	unicode := int(ret)
 	utf8 := unicode2utf8(unicode)
 
-	return gb2312, utf8, nil
+	return gb2312, unicode, utf8, nil
 }
--- gb2312_test.go
+++ gb2312_test.go
@@ -2,15 +2,36 @@ package gogb2312
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
-func print_utf8(u8 int) {
+func hexprint_utf8(u8 int) {
 	fmt.Printf("%x %x %x\n", u8>>16, (u8>>8)&0xff, u8&0xff)
 }
 
+func hexprint_utf8string(s string) {
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+}
+
+func hexprint_bytes(s []byte) {
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+}
+
 func Test_unicode2utf8(t *testing.T) {
-	print_utf8(unicode2utf8(0x90ed))
+	s := unicode2utf8(0x90ed)
+	hexprint_utf8(s)
+	b := make([]byte, 3)
+	b[0] = byte(s >> 16)
+	b[1] = byte((s >> 8) & 0xff)
+	b[2] = byte(s & 0xff)
+	fmt.Println(string(b))
 }
 
 func Test_RE(t *testing.T) {
@@ -31,6 +52,24 @@ func Test_RE(t *testing.T) {
 	_ = b
 }
 
+func Test_MakeData(t *testing.T) {
+	//readcp936("./CP936.TXT")
+}
+
 func Test_Convert(t *testing.T) {
-	readcp936("./CP936.TXT")
+	buf, err := ioutil.ReadFile("./test1.txt")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	hexprint_bytes(buf)
+	output, cerr, il, ol := ConvertGB2312(buf)
+	if cerr != nil {
+		t.Error(cerr.Error())
+	}
+	_ = il
+	_ = ol
+	hexprint_utf8string("郭铁")
+
+	hexprint_bytes(output)
+	fmt.Printf("%s\n", output)
 }
